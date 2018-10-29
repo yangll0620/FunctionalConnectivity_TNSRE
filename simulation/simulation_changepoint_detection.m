2fe94694be6427f927a5b0c9ecc062fcfafd423e
yll0620@gmail.com <yll0620@gmail.com>
2018-10-29 16:12:55 +0800

    added plot figure,detect change points on simulation data

--- simulation/simulation_changepoint_detection.m
+++ simulation/simulation_changepoint_detection.m
@@ -3,5 +3,7 @@ function points_change  = simulation_changepoint_detection()
 addpath('..\')
 
 load('.\matrixConn_Surrogate.mat'); % load matrix_Con_Surr
-points_change = changepoint_detection_cosSimilarity(matrix_Con_Surr);
-
+[points_change,diff] = changepoint_detection_cosSimilarity(matrix_Con_Surr);
+plot(diff)
+saveas(gcf, 'cosSimilarity.png','png')
+disp(['change points are  ' num2str(points_change')])
