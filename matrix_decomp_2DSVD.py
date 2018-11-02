9f5e634a0dcb437baa288b1aff9c577cb45dfa5d
yll0620@gmail.com <yll0620@gmail.com>
2018-11-02 15:06:18 +0800

    matrix decomposition using 2DSVD, have doubts

---
+++ matrix_decomp_2DSVD.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: yll
+# @Date:   2018-10-26 10:38:51
+# @Last Modified by:   yll
+# @Last Modified time: 2018-10-26 10:47:49
+
+
+def matrix_decomp_2DSVD():
+# decompose matrix using 2DSVD
