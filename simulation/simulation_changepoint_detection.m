5c256128e739a8e337210ea0c797701deae55c77
yll0620@gmail.com <yll0620@gmail.com>
2018-11-02 14:47:37 +0800

    change point detection on simulated data

--- simulation/simulation_changepoint_detection.m
+++ simulation/simulation_changepoint_detection.m
@@ -1,5 +1,5 @@
 function points_change  = simulation_changepoint_detection()
-% detect the change points using the simulation dataset
+% detect the change points on the simulation dataset
 addpath('..\')
 
 load('.\matrixConn_Surrogate.mat'); % load matrix_Con_Surr
