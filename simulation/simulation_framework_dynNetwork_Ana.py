b324cca09b06d917d873509453decc5e6fb53f00
yll0620@gmail.com <yll0620@gmail.com>
2018-11-12 16:05:38 +0800

    new simulation codes with 3 segments

--- simulation/simulation_framework_dynNetwork_Ana.py
+++ simulation/simulation_framework_dynNetwork_Ana.py
@@ -2,7 +2,7 @@
 # @Author: yll
 # @Date:   2018-10-31 16:13:41
 # @Last Modified by:   yll
-# @Last Modified time: 2018-11-02 12:08:18
+# @Last Modified time: 2018-11-08 15:43:42
 
 import pandas as pd
 import numpy as np
@@ -10,10 +10,12 @@ from scipy.io import loadmat
 
 import sys
 sys.path.append("../")
+from changepoint_detection_cosSimilarity import changepoint_detection_cosSimilarity
 from community_detection import community_detection
 from network_visualization import network_visualization
 from weight_for_plotting_gen import weight_for_plotting_gen
-from representive_connMatrix_gen import representive_connMatrix_tucker
+from representive_connMatrix_gen import representive_connMatrix_tucker, representive_connMatrix_SVD
+
 
 '''
 	framework for the whole dynamic network analysis on the simulated connectivity matrix
@@ -29,22 +31,28 @@ data = loadmat('./matrixConn_Surrogate.mat')
 matrix_Con_Surr = data['matrix_Con_Surr'] # matrix_Con_Surr: n_chns * n_chns * n_times
 
 # read the channel location informations 
-inf_chans = pd.read_csv('acticap64xy.csv', sep=' ', index_col = False, header = None).values # inf_chans: n_chns * 4
-idx_name = 0  # inf_chns column 0: the channel index (0,1,2...), column 3: channel names ('FP1' et.al)
+inf_chans = pd.read_csv('./xy.csv', sep=',', index_col = False, header = None).values # inf_chans: n_chns * 4
+idx_name = 0  # inf_chns column 0: 0,1,2,3..., column 3: channel names ('FP1' et.al), column 4: the channel index (0,1,2...)
 name = inf_chans[:,idx_name] 
 coord_xy = inf_chans[:,1:3] # coord_xy:n_chans * 2, inf_chans column 1 & 2: x,y coordinates in planar 
 
 # detect change points
 ''' separate EEG into individal segments'''
-conn_seg = matrix_Con_Surr[:,:,0:60] # individual segment
+points_change = changepoint_detection_cosSimilarity(matrix_Con_Surr)
+i_pointchange = 0
+point_change = points_change[i_pointchange]
+conn_seg = matrix_Con_Surr[:,:,121:-1] # individual segment
+np.savetxt("changepoint.txt",points_change)
 
 ''' extract the representive connectivity matrix for each time segment'''
-connM_repre = representive_connMatrix_tucker(conn_seg)
+connM_repre = representive_connMatrix_SVD(conn_seg)
+np.savetxt("connM_repre.txt",connM_repre)
+
 '''detect community'''
 membership = community_detection(connM_repre)
 '''Network Visualization '''
 # generate new connective matrix for just showing the most significant edges with p = p_sig
-p_sig = 0.95
+p_sig = 0.75
 w_plot = weight_for_plotting_gen(connM_repre, p_sig)
 # visualize the network
 network_visualization(w_plot, membership, name, coord_xy)
