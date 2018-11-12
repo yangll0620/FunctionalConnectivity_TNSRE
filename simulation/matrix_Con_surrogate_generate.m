5e50b48e0e045f91c88124f24e38b4e9cb5e44fb
yll0620@gmail.com <yll0620@gmail.com>
2018-11-12 16:00:51 +0800

    three segments, each segment has 2-3 clusters

--- simulation/matrix_Con_surrogate_generate.m
+++ simulation/matrix_Con_surrogate_generate.m
@@ -24,7 +24,6 @@ for i_time = 1: n_time
     end
 end
 
-
 % in time [1:60]
 ind_Cluster = [1:30]; % total edges = nchoosek(30,2) = 435
 for i_time = 1:change_point1-1
@@ -33,8 +32,8 @@ for i_time = 1:change_point1-1
         for j = i+1:length(ind_Cluster)
             j_chn = ind_Cluster(j);
             
-            mu = rand(1)*0.4+0.2;
-            weight = normrnd(mu,0.01); % mu in [0.2,0.6] 
+            mu = rand(1)*0.4+0.3;
+            weight = normrnd(mu,0.01); % mu in [0.3,0.7] 
             matrix_Con_Surr(i_chn,j_chn,i_time) = weight; 
             matrix_Con_Surr(j_chn,i_chn,i_time) = weight; 
             clear i_chn_inner mu weight j_chn
@@ -44,17 +43,36 @@ for i_time = 1:change_point1-1
     clear i
 end
 clear ind_Cluster i_time
+ind_Cluster = [31:64]; % total edges = nchoosek(20,2) = 435
+for i_time = 1:change_point1-1
+    for i = 1:length(ind_Cluster)
+        i_chn = ind_Cluster(i);
+        for j = i+1:length(ind_Cluster)
+            j_chn = ind_Cluster(j);
+            
+            mu = rand(1)*0.4+0.3;
+            weight = normrnd(mu,0.01); % mu in [0.3,0.7] 
+            matrix_Con_Surr(i_chn,j_chn,i_time) = weight; 
+            matrix_Con_Surr(j_chn,i_chn,i_time) = weight; 
+            clear i_chn_inner mu weight j_chn
+        end 
+        clear i_chn j
+    end
+    clear i
+end
+clear ind_Cluster i_time
+% %%%%%%%%%%% in time [1:60]
 
 % in [61:120]
-ind_Cluster = [16:48]; % total edges = nchoosek(30,2) = 435
+ind_Cluster = [1:15]; % total edges = nchoosek(30,2) = 435
 for i_time = change_point1 : change_point2-1
     for i = 1:length(ind_Cluster)
         i_chn = ind_Cluster(i);
         for j = i+1:length(ind_Cluster)
             j_chn = ind_Cluster(j);
             
-            mu = rand(1)*0.4+0.2;
-            weight = normrnd(mu,0.01); % mu in [0.2,0.6] 
+            mu = rand(1)*0.4+0.3;
+            weight = normrnd(mu,0.01); % mu in [0.3,0.7] 
             matrix_Con_Surr(i_chn,j_chn,i_time) = weight; 
             matrix_Con_Surr(j_chn,i_chn,i_time) = weight;  
             clear i_chn_inner mu weight j_chn
@@ -63,17 +81,52 @@ for i_time = change_point1 : change_point2-1
     end
 end
 clear ind_Cluster i_time
+ind_Cluster = [16:48]; % total edges = nchoosek(30,2) = 435
+for i_time = change_point1 : change_point2-1
+    for i = 1:length(ind_Cluster)
+        i_chn = ind_Cluster(i);
+        for j = i+1:length(ind_Cluster)
+            j_chn = ind_Cluster(j);
+            
+            mu = rand(1)*0.4+0.3;
+            weight = normrnd(mu,0.01); % mu in [0.3,0.7] 
+            matrix_Con_Surr(i_chn,j_chn,i_time) = weight; 
+            matrix_Con_Surr(j_chn,i_chn,i_time) = weight;  
+            clear i_chn_inner mu weight j_chn
+        end 
+        clear i_chn j
+    end
+end
+clear ind_Cluster i_time
+ind_Cluster = [49:64]; % total edges = nchoosek(30,2) = 435
+for i_time = change_point1 : change_point2-1
+    for i = 1:length(ind_Cluster)
+        i_chn = ind_Cluster(i);
+        for j = i+1:length(ind_Cluster)
+            j_chn = ind_Cluster(j);
+            
+            mu = rand(1)*0.4+0.3;
+            weight = normrnd(mu,0.01); % mu in [0.3,0.7] 
+            matrix_Con_Surr(i_chn,j_chn,i_time) = weight; 
+            matrix_Con_Surr(j_chn,i_chn,i_time) = weight;  
+            clear i_chn_inner mu weight j_chn
+        end 
+        clear i_chn j
+    end
+end
+clear ind_Cluster i_time
+% %%%%%%%%%%%%%%%%% in [61:120]
 
 % in [121:180]
-ind_Cluster = [45:64]; % total edges = nchoosek(30,2) = 435
+ind_Cluster = [1:44]; % total edges = nchoosek(30,2) = 435
 for i_time = change_point2 : n_time
     for i = 1:length(ind_Cluster)
         i_chn = ind_Cluster(i);
         for j = i+1:length(ind_Cluster)
             j_chn = ind_Cluster(j);
             
-            mu = rand(1)*0.4+0.2;
-            weight = normrnd(mu,0.01); % mu in [0.2,0.6] 
+            mu = rand(1)*0.4+0.3;
+            weight = normrnd(mu,0.01); % mu in [0.3,0.6] 
             matrix_Con_Surr(i_chn,j_chn,i_time) = weight; 
             matrix_Con_Surr(j_chn,i_chn,i_time) = weight; 
             clear i_chn_inner mu weight j_chn
@@ -82,6 +135,24 @@ for i_time = change_point2 : n_time
     end
 end
 clear ind_Cluster i_time
+ind_Cluster = [45:64]; % total edges = nchoosek(30,2) = 435
+for i_time = change_point2 : n_time
+    for i = 1:length(ind_Cluster)
+        i_chn = ind_Cluster(i);
+        for j = i+1:length(ind_Cluster)
+            j_chn = ind_Cluster(j);
+            
+            mu = rand(1)*0.4+0.3;
+            weight = normrnd(mu,0.01); % mu in [0.3,0.6] 
+            matrix_Con_Surr(i_chn,j_chn,i_time) = weight; 
+            matrix_Con_Surr(j_chn,i_chn,i_time) = weight; 
+            clear i_chn_inner mu weight j_chn
+        end 
+        clear i_chn j
+    end
+end
+clear ind_Cluster i_time
+% %%%%%%%%%%%%%%%%% in [121:180]
 
 save('matrixConn_Surrogate.mat','matrix_Con_Surr')
 
