9f0ba68c40a469609c82b88374c3b3f56a365c3b
yll0620@gmail.com <yll0620@gmail.com>
2018-11-12 16:07:15 +0800

    adjust the weight of the showed edges

--- weight_for_plotting_gen.py
+++ weight_for_plotting_gen.py
@@ -2,7 +2,7 @@
 # @Author: yll
 # @Date:   2018-11-02 11:35:51
 # @Last Modified by:   yll
-# @Last Modified time: 2018-11-02 12:06:25
+# @Last Modified time: 2018-11-08 15:30:00
 
 from scipy import stats
 import numpy as np
@@ -21,6 +21,6 @@ def weight_for_plotting_gen(matrix_conn, p_sig):
 	shape, loc, scale = stats.lognorm.fit(matrix_conn.flatten()) # fit a lognorm distribution for all the connectivity weights 
 	pval = stats.lognorm.cdf(matrix_conn.flatten(order='c'), s=shape, loc=loc, scale=scale) # Cumulative distribution function. pval: (n_chns*n_chns,)
 	pval = np.reshape(pval, matrix_conn.shape, order='c') # pval: (n_chns,n_chns)
-	w_plot = matrix_conn * (pval>p_sig).astype(float) # w_plot: the weights of significant edges remains, the weights of the insignificant edges = 0
-
+	#w_plot = matrix_conn * (pval>p_sig).astype(float) # w_plot: the weights of significant edges remains, the weights of the insignificant edges = 0
+	w_plot = matrix_conn * (matrix_conn>0.25).astype(float) # w_plot: the weights of significant edges remains, the weights of the insignificant edges = 0
 	return w_plot
